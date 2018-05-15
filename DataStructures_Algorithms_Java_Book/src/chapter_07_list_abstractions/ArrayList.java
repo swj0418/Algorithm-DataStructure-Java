ca475ac7795a33400b57ed28a1947cf28691de1c
Sangwon Jeong <top1@Sangwons-MacBook-Pro.local>
2018-05-15 12:41:14 +0900

    Commit from Mac

--- DataStructures_Algorithms_Java_Book/src/chapter_07_list_abstractions/ArrayList.java
+++ DataStructures_Algorithms_Java_Book/src/chapter_07_list_abstractions/ArrayList.java
@@ -33,17 +33,35 @@ public class ArrayList<E> implements List<E> {
 
     @Override
     public E set(int i, E e) throws IndexOutOfBoundsException {
-        return null;
+        checkIndex(i, size);
+        E temp = data[i];
+        data[i] = e;
+        return temp;
     }
 
     @Override
     public void add(int i, E e) throws IndexOutOfBoundsException {
-
+        checkIndex(i, size + 1);
+        if(size == data.length) {
+            throw new IllegalStateException("Array is full");
+        }
+        for(int k = size - 1; k >= i; k--) {
+            data[k + 1] = data[k];
+        }
+        data[i] = e;
+        size++;
     }
 
     @Override
     public E remove(int i) throws IndexOutOfBoundsException {
-        return null;
+        checkIndex(i, size);
+        E temp = data[i];
+        for(int k = i; k < size - 1; k++) {
+            data[k] = data[k + 1];
+        }
+        data[size - 1] = null;
+        size--;
+        return temp;
     }
 
     private void checkIndex(int i, int n) throws IndexOutOfBoundsException {
